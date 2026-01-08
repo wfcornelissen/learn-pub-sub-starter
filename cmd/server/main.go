051e8fe5d830654b28ca1fdf7147b72e9603abda
wfcornelissen <wfcornelissen@gmail.com>
2026-01-08 16:54:36 +0200

    Implemented PublishJSON in main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
@@ -14,6 +16,7 @@ func main() {
 	conn, err := amqp.Dial(connString)
 	if err != nil {
 		fmt.Printf("Failed to establish connection:/n%v/n", err)
+		return
 	}
 	defer conn.Close()
 	fmt.Println("Connection successfully established!")
@@ -21,6 +24,13 @@ func main() {
 	connChannel, err := conn.Channel()
 	if err != nil {
 		fmt.Printf("Failed to create a channel for the connection:/n%v/n", err)
+		return
+	}
+
+	err = pubsub.PublishJSON(connChannel, routing.ExchangePerilDirect, string(routing.PauseKey), routing.PlayingState{IsPaused: true})
+	if err != nil {
+		fmt.Printf("Failed to publish:/n%v/n", err)
+		return
 	}
 
 	signalChan := make(chan os.Signal, 1)
